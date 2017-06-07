4dac9c09ac52b5de8328af41ac3fca313ed8ba25
Milan Thummar <Milan Thummar>
2017-06-08 00:18:22 +0530

    new file:   build.gradle 	new file:   gradle/wrapper/gradle-wrapper.jar 	new file:   gradle/wrapper/gradle-wrapper.properties 	new file:   gradlew 	new file:   gradlew.bat 	new file:   src/main/java/course/application/web/CourseWebAppApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/course/application/web/CourseWebAppApplicationTests.java

---
+++ src/main/java/course/application/web/CourseWebAppApplication.java
@@ -0,0 +1,12 @@
+package course.application.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWebAppApplication.class, args);
+	}
+}
